d1e6386796c933496ad2415fabcb333482edf52d
Iresh Sharma <iresharma@MACBOOKs-MacBook-Pro.local>
2019-08-02 00:25:36 +0530

    Input

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class input {
+	public void main() {
+		Scanner sc =new Scanner(System.in);
+		system.out.println("Please Enter your Name")
+		String = sc.nextInt();
+	}
+}
