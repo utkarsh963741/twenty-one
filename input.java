0f58ec8db936d20d4fd5500426bceae17aa61a14
utkarsh963741 <51253240+utkarsh963741@users.noreply.github.com>
2019-08-02 00:33:38 +0530

    Update input.java

--- input.java
+++ input.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class input {
 	public void main() {
-		Scanner sc =new Scanner(System.in);
 		system.out.println("Please Enter your Name")
 		String = sc.nextInt();
 	}
